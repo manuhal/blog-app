97e46fef21004de0558fa297905d195fd3a642a1
Manu <44656862+manuelhal@users.noreply.github.com>
2019-05-10 23:21:05 -0700

    add CRUD operation with forms

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 # blog/models.py
 
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -11,5 +12,10 @@ class Post(models.Model):
     )
     body = models.TextField()
 
+
     def __str__(self):
         return self.title
+
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
