82dea90a835b3b782f969a0c087355983d451314
Manu <manuelhal@outlook.com>
2019-05-11 21:31:41 -0700

    add app: accounts & add html-template: signup, login

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 # blog/views.py
 
-# from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
